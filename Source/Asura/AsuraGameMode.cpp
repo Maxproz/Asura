96f641eedea22f5dab40009b106aada745cf504a
Maxpro-PC\Maxpro <maxdietz@hotmail.com>
2018-01-02 07:33:12 -0800

    Initial commit

---
+++ Source/Asura/AsuraGameMode.cpp
@@ -0,0 +1,22 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "AsuraGameMode.h"
+#include "AsuraPlayerController.h"
+#include "AsuraCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AAsuraGameMode::AAsuraGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = AAsuraPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+
+
+}
---
+++ Source/Asura/AsuraGameMode.h
@@ -0,0 +1,24 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AsuraGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAsuraGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+
+public:
+
+	AAsuraGameMode();
+
+
+
+};
+
+
+
